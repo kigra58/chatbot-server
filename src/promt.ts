96f5b7d6ae76255cafd30fab6c21e90f8c32878e
Krishna-Biz4-at-338516037032 <krishna@biz4group.com>
2025-01-17 17:00:49 +0530

    update code

--- src/promt.ts
+++ src/promt.ts
@@ -1,21 +1,61 @@
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+// import {config} from "dotenv"
+
+// config();
+
+
+// const googleAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
+// const geminiModel = googleAI.getGenerativeModel({
+//   model: "gemini-1.5-flash",
+// });
+
+// export const generate = async (question:string) => {
+//     try {
+//       const result = await geminiModel.generateContent(question);
+//       const response = result.response;
+//       console.log(response.text());
+//       return response.text();
+//     } catch (error) {
+//       console.log("response error", error);
+//     }
+//   };
+
+
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import {config} from "dotenv"
+import { config } from "dotenv";
 
 config();
-
 
 const googleAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
 const geminiModel = googleAI.getGenerativeModel({
   model: "gemini-1.5-flash",
 });
 
-export const generate = async (question:string) => {
-    try {
-      const result = await geminiModel.generateContent(question);
-      const response = result.response;
-      console.log(response.text());
-      return response.text();
-    } catch (error) {
-      console.log("response error", error);
+export const generate = async (question: string) => {
+  try {
+    const result = await geminiModel.generateContent(question);
+    const response = result.response;
+    console.log(response.text());
+    return response.text();
+  } catch (error) {
+    console.log("response error", error);
+  }
+};
+
+export const generateStream = async (question: string) => {
+  try {
+    const result = await geminiModel.generateContentStream(question);
+    
+    let fullResponse = '';
+    
+    for await (const chunk of result.stream) {
+      const chunkText = chunk.text();
+      // console.log(chunkText);
+      fullResponse += chunkText;
     }
-  };
+    
+    return fullResponse;
+  } catch (error) {
+    console.log("stream error", error);
+  }
+};
